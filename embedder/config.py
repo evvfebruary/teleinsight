e1ebd831310b4757d1d55c2a46c75833558cc4a8
evv <evv.bmstu@gmail.com>
2024-09-20 23:45:06 +0300

    chore: use some envs variables

--- embedder/config.py
+++ embedder/config.py
@@ -1,5 +1,11 @@
-KAFKA_HOST = "rc1a-k33qdb97nnq4iubp.mdb.yandexcloud.net"
-KAFKA_PORT = 9091
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+
+KAFKA_HOST = os.getenv("KAFKA_HOST")
+KAFKA_PORT = os.getenv("KAFKA_PORT")
 CONSUMER_GROUP = "teleinsight-new-images-getters"
 CLIP_MODEL_NAME = "openai/clip-vit-base-patch32"
 TOPIC_WITH_NEW_ATTACHMENTS = "freshly_created_attachments"
@@ -8,3 +14,7 @@ TOPIC_WITH_NEW_ATTACHMENTS = "freshly_created_attachments"
 INPUT_TOPIC = "freshly_created_attachments"
 OUTPUT_TOPIC = "embeddings_out"
 BUCKET_NAME = "teleinsight"
+
+
+sasl_username = os.getenv("SASL_USERNAME")
+sasl_password= os.getenv("SASL_PASSWORD")
